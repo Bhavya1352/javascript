3803e2251c2fa8a3cf5669d933a5803bac30b529
Bhavya1352 <bhavyamishra698@gmail.com>
2025-03-25 16:31:07 +0000

    q,,

--- basics/variable.js
+++ basics/variable.js
@@ -1,4 +1,20 @@
 const accountId = 11453
-accountId= 2
+// accountId= 2  not allowed ,constant cannot be changed
 
-console.log(accountId);
+// console.log(accountId); 
+let accountEmail ="bhavya@njfnjf.com"
+var accountPassword = "12333333333"
+  accountCity="Jaipur"
+  let accountState;
+
+
+     accountEmail ="hc.hc@com"
+     accountPassword="7685398858"
+     accountCity="benglali"
+
+     console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+         /*
+         var prefer not to use due to block scope and functional scope
+         /*
+
+
