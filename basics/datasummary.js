44eaa3c4f410d034f545ba38e785e01ef007cae1
Bhavya1352 <bhavyamishra698@gmail.com>
2025-03-27 16:52:50 +0000

    f

---
+++ basics/datasummary.js
@@ -0,0 +1,11 @@
+// datatype two type
+// primitive- call by value 
+// 7 Types- string,number boolean null undefined symbol bigint
+// non primitive- refrence type
+//  object array function
+
+// javascript is dynamic because it can hold variable of diffrent datatypes during runtime.
+const id =symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id==anotherId);
