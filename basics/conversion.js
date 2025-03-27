509d9353073c77c31bd0b55543103ab1c36a7901
Bhavya1352 <bhavyamishra698@gmail.com>
2025-03-27 15:28:36 +0000

    jkbjbj

---
+++ basics/conversion.js
@@ -0,0 +1,8 @@
+// "33" == 33
+// "33abc" = Nam
+// true =1
+// false=0
+// 1 = true
+// 0 = false
+// if empty string == false
+// "name or anything inside it" = true
---
+++ basics/datatype.js
@@ -0,0 +1,15 @@
+// "use strict" treat all in new js code version
+// code should be readable 
+
+// DATATYPES
+// NUMBER--- 2^N
+// BigInt-- large number
+// string- use in double cote("")
+// boolean- true/false
+//  null- it is not undefined it is empty, standalone value
+// undefined- variable declared but value not assigned
+// symbol- unique
+
+// datatype from 4 to 11 are primitive datatype
+
+// non primitve- Object
