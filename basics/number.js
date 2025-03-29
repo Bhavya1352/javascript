29ddd3b895250dff3c3080c8a4bb603abf48132a
Bhavya1352 <bhavyamishra698@gmail.com>
2025-03-29 18:52:14 +0000

    grrg

--- basics/number.js
+++ basics/number.js
@@ -5,5 +5,14 @@ const score =400
 // console.log(balance);
 
 const number= 238.944
-console.log(number.toprecision());
+// console.log(number.toPrecision(3));
+// console.log(Math);
+// console.log(Math.round(4.33));
+// console.log(Math.ceil(4.2));
+// console.log(Math.abs(3));
+console.log(Math.random());
+
+
+
+
 
