32dae1253c9d1c45e2a9ccfa6380f3b598cf53e5
Bhavya1352 <bhavyamishra698@gmail.com>
2025-03-29 15:51:17 +0000

    jbjsb

--- basics/datasummary.js
+++ basics/datasummary.js
@@ -5,7 +5,7 @@
 //  object array function
 
 // javascript is dynamic because it can hold variable of diffrent datatypes during runtime.
-const id =symbol('123')
-const anotherId =Symbol('123')
+const id = Symbol('123')
+const anotherId = Symbol('123')
 
-console.log(id==anotherId);
+console.log(id===anotherId);
