eaa01eef2be8bb975b1021f09a2319da55db4e89
Bhavya1352 <bhavyamishra698@gmail.com>
2025-03-29 17:00:31 +0000

    dd

--- basics/string.js
+++ basics/string.js
@@ -1,5 +1,22 @@
-const name= "bhavya"
-const repo = "5"
-console.log('hello my name is ${name} and my repocount is ${repo}');
+const name = "bhavya"
+const repo = 6
+// console.log (`hello my name is ${name} and my repocount is ${repo}`);
+// code written in 3 line is the way to print String
 
 // string in double cote""
+const gameName = new String('bhavyaa')
+// console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('v'));
+const newString= gameName.substring(0,2)
+console.log(newString);
+// slice can include negative
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+
+
+
+
+
