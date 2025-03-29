9aa2bbafeb7f504f2a567c4d140d007dea587232
Bhavya1352 <bhavyamishra698@gmail.com>
2025-03-29 19:31:15 +0000

    hsbshb

--- basics/dates.js
+++ basics/dates.js
@@ -9,8 +9,11 @@ let myDate = new Date()
 let createdDate = new Date(2023, 0, 23)
 // console.log(createdDate.toLocaleDateString())
 // console.log(createdDate.toString());
-let item = new Date("2023-02-13")
+// let item = new Date("2023-02-13")
+let item = new Date("14-02-2023")
 console.log(item);
+console.log(item.toLocaleDateString());
+
 
 // 0- january 1-february ........
 
