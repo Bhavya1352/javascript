75c28ee603aa2c2f910b25f2a257e91d3ec30cc9
Bhavya1352 <bhavyamishra698@gmail.com>
2025-03-29 18:28:44 +0000

    r

---
+++ basics/number.js
@@ -0,0 +1,9 @@
+const score =400
+// console.log(score);
+
+// const balance= new Number(100); modern method to write
+// console.log(balance);
+
+const number= 238.944
+console.log(number.toprecision());
+
