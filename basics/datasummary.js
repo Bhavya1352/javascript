020501feec16f2a9c13f5dcf748a127235be30ae
Bhavya1352 <bhavyamishra698@gmail.com>
2025-03-29 16:21:35 +0000

    djdb

--- basics/datasummary.js
+++ basics/datasummary.js
@@ -2,10 +2,15 @@
 // primitive- call by value 
 // 7 Types- string,number boolean null undefined symbol bigint
 // non primitive- refrence type
-//  object array function
+//  object ,array, function
 
 // javascript is dynamic because it can hold variable of diffrent datatypes during runtime.
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
 console.log(id===anotherId);
+
+// memory types- stack (primitve) - we will get variable copy and Heap (non primitive)- we get refrence of orignal value
+let youtubename = bhavya.com
+let anothername = youtubename
+console.log(anothername);
