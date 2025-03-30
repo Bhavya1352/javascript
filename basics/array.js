dafb2c2eb9cd677f94c04a07321c81fb56856c48
Bhavya1352 <bhavyamishra698@gmail.com>
2025-03-30 19:39:23 +0000

    m m, m

--- basics/array.js
+++ basics/array.js
@@ -1,10 +1,45 @@
-const myarr =[0,1,2,3,4,5]
-const myhero = [shaktiman,naagraj]
+const myArr =[0,1,2,3,4,5]
+// const myhero = [shaktiman,naagraj]
 // way to write array-- const myarr = new Array(0,1,2,3)
-const myarr2 = new Array(0,1,2,3,4)
+// const myarr2 = new Array(0,1,2,3,4)
 // console.log(myarr2[0]);
 
 // array methods------
-myarr.push(6)
-console.log(myarr);
+// myArr.push(6)
+// myArr.push(8) 
+// myArr.pop()
+// myArr.unshift(0)
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(19));
+
+
+// console.log(myArr);
+// console.log(myArr);
+// push- add elements in array
+// pop- removes last element
+// const newArr =myArr.join()
+// console.log(newArr);
+// console.log(myArr);
+// console.log(typeof newArr);
+// slice and splice 
+console.log("A ", myArr)
+
+const myn1 =myArr.slice(1,3)
+console.log(myn1);
+console.log("B ",myArr);
+
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
+console.log("C ",myArr);
+
+// slice-- copy of array
+// splice- changes are made in array and new generated
+
+
+
+
+
+
 
