5da95036057db36ad25a932b40534551d90d6e2f
Bhavya1352 <bhavyamishra698@gmail.com>
2025-04-02 12:38:20 +0000

    mn

---
+++ basics/scope.js
@@ -0,0 +1,16 @@
+// {} ----this means scope ,,if{}---iske andar content ko bolte block scope
+// if ke bahar wale ko bolte haii global scope 
+
+// let a = 10
+const b = 20
+var c = 30
+if(true) {
+    // let a =2000
+    var c = 3000
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
