a3463fb79533b1afec4cb70ba3d54894107c8810
Bhavya1352 <bhavyamishra698@gmail.com>
2025-04-02 19:57:14 +0000

    ijoij

--- basics/arrow.js
+++ basics/arrow.js
@@ -20,8 +20,26 @@ const user = {
 // }
 // chai()
 
-   const chai = function(){
-    let userName = "bhavya"
-    console.log(this.username)
+//    const chai = function() {
+    let userName = "bhavya" ;
+    // console.log (this.username);
+// }
+
+const chai = () => {
+    let userName = "bhavya" 
+    // console.log (this.username);
 }
+// function have their own this content 
+// but arrow function does not have 
+
+// const addTwo =(num1 ,num2) =>{
+    // return num1 + num2
+// }
+// console.log(addTwo (3,4))
+
+// 0r  below given is another way of adding
+// most used method it is mainly in react
+const addTwo = (num1 ,num2) => num1 + num2
+console.log(addTwo (3,4))
+
 
