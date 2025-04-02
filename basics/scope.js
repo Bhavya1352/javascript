2bc4c1ffe5244c4c30967c76c6d7215a507a8a2b
Bhavya1352 <bhavyamishra698@gmail.com>
2025-04-02 13:37:25 +0000

    kki

--- basics/scope.js
+++ basics/scope.js
@@ -1,16 +1,31 @@
 // {} ----this means scope ,,if{}---iske andar content ko bolte block scope
 // if ke bahar wale ko bolte haii global scope 
 
-// let a = 10
+let a = 10
 const b = 20
 var c = 30
 if(true) {
-    // let a =2000
-    var c = 3000
+    let a = 2000
+    // var c = 3000
+    const b = 2000
+    console.log("INNER :" ,a)
 }
 
 // console.log(a);
-// console.log(b);
-console.log(c);
+// console.log (b);
+// console.log (c);
 
+// //
+  function one(){
+    const username = "Bhavya"
+  function two(){
+    // const website = "youtube"
+  console.log(username);
+   }
+    // console.log(website); 
+    // -----this line will show error as bracket is closed and 
+    // similarly two will also not execute
 
+    two()
+}
+one()
