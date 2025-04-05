2150503d00683d999314eff889626988f4b8ac9c
Bhavya1352 <bhavyamishra698@gmail.com>
2025-04-05 14:04:53 +0000

    jjejwnj

---
+++ basics/one.js
@@ -0,0 +1,63 @@
+// if   {}--- iske beech me scope
+if(true){       
+// if condition true tbhi bracket ke andar ka code dekhenge
+}
+if(false){
+    // agar condition false dii hai toh bracket ke andar ka code nahi 
+    // dikhenge {}- iske andar wala ignore 
+}
+// const isUserloggedIn =true
+// if(isUserloggedIn){
+
+// }
+
+// comparison-  ==(means to check value) < ,> <= ,>= ,== ,!= ,===(type check)
+const temperature = 41
+if (temperature === 40){
+console.log ("temperature less than 50")
+} else{
+
+// console.log ("temperature greater than 50")
+}
+// console.log( "execute")
+// const balance =1000
+// if (balance > 500) ; console.log("test1") ,console.log("test2");  dont write like this 
+// there should be readiablitly in code 
+// way is -
+//  const balance = 1000;
+// if (balance > 500); console.log("test1") ,
+// console.log("test2");
+
+// const balance = 1000
+// if(balance< 500) {  
+    // console.log("less than 500");
+    // 
+// }else if (balance <750){ 
+// console.log("less than 750");
+
+// }else if( balance< 950){
+    // console.log("less than 950");
+    
+// }
+// else {
+    // console.log("less than 1200");
+    
+// }
+// && -- this tells have to check all in that()
+// || - it tells or in checking mtlb 2 option me se koi ek
+
+const userLoggedIn = true
+const debitCard = true 
+const partion =true
+const mentor = false
+// if (userLoggedIn && debitCard){
+    // console.log("user is allowed for shopping");
+    
+// }
+if (partion || mentor) {
+    console.log("user is allowed");
+
+}
+  
+
+
