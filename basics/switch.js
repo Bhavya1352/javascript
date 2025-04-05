ac57d76f0ce48765803b19164e3d1b0911c83bea
Bhavya1352 <bhavyamishra698@gmail.com>
2025-04-05 14:19:07 +0000

    kolfe

---
+++ basics/switch.js
@@ -0,0 +1,36 @@
+ const month = 3
+ switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("february");
+        
+        break;
+ 
+    case 2:
+        console.log("february");
+        
+        break;
+        case 3:
+        console.log("march");
+        
+        break;
+
+        case 4:
+        console.log("april");
+        
+        break;
+ 
+ 
+    default:
+        console.log("default case match");
+        
+        break;
+//  if we want to copy content use 
+// (shift+ alt+ down arrow ) together content will copied or want duplicate
+
+// break me jahan humara answer match ho jata hai uske baad sab print hota hai 
+// except default 
+// nmv  
